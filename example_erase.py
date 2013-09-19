2f8a9905db75a7cf627305a4153cbd52dc14523a
Dan Michael O. Heggø <danmichaelo@gmail.com>
2013-09-19 13:48:36 +0200

    Add erase example

---
+++ example_erase.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+import logging
+import thread
+import time
+import json
+import argparse
+import yaml
+import time
+from copy import copy
+
+from pyrfidgeek import PyRFIDGeek
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+logger.addHandler(ch)
+
+parser = argparse.ArgumentParser(description='PyRfidGeek write patron card example')
+parser.add_argument('--config', nargs='?', default='config.yml', help='Config file')
+args = parser.parse_args()
+config = yaml.load(open(args.config, 'r'))
+
+rfid = PyRFIDGeek(config)
+uids = list(rfid.inventory())
+if len(uids) == 1:
+	rfid.enable_led(5)
+	tag = rfid.erase_card(uids[0])
+	rfid.disable_led(5)
+else:
+	logger.error('Found %d tags, not 1' % (len(uids)))
