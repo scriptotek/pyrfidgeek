03262a5b501b01d2d60d8ca69914418e3db80ea7
Dan Michael O. Heggø <danmichaelo@gmail.com>
2017-04-07 19:39:24 +0200

    Add multi-protocol inventory example

---
+++ example_inventory.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+import yaml
+from rfidgeek import PyRFIDGeek, ISO14443A, ISO15693
+
+config = yaml.load(open('config.yml', 'r'))
+reader = PyRFIDGeek(config)
+
+protocols = [ISO14443A, ISO15693]
+
+try:
+    while True:
+        tags = {protocol: set() for protocol in protocols}
+
+        for protocol in protocols:
+            reader.set_protocol(protocol)
+            for uid in reader.inventory():
+                tags[protocol].add(uid)
+
+        tags2 = ['{} ({})'.format(tag, protocol) for protocol in protocols for tag in tags[protocol]]
+        total_tags = len(tags[ISO14443A]) + len(tags[ISO15693])
+
+        print('Found %d tag(s): %s' % (total_tags, ', '.join(tags2)))
+
+finally:
+    print('Bye!')
+    reader.close()
