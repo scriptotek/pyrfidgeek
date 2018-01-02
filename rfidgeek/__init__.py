ee6fc627c310018e150f8d7af75fd26ab45ab501
Dan Michael O. Heggø <danmichaelo@gmail.com>
2018-01-02 23:15:33 +0100

    Expose __version__

--- rfidgeek/__init__.py
+++ rfidgeek/__init__.py
@@ -2,6 +2,9 @@ import logging
 from .rfidgeek import PyRFIDGeek, ISO14443A, ISO14443B, ISO15693
 from .crc import CRC
 
+import pkg_resources  # part of setuptools
+__version__ = pkg_resources.require('rfidgeek')[0].version
+
 # Logging: Add a null handler to avoid "No handler found" warnings.
 try:
     from logging import NullHandler
